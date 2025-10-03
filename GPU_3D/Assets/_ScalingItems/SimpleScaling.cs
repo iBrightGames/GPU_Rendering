39168f3e74626cf0ab6b0b9b3545ce12edf4da49
thehouseofcolors <rengin.celik97@gmail.com>
2025-10-03 15:42:16 +0300

    SimpleScaling works

--- GPU_3D/Assets/_ScalingItems/SimpleScaling.cs
+++ GPU_3D/Assets/_ScalingItems/SimpleScaling.cs
@@ -1,10 +1,10 @@
+
 using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class SimpleScaling : MonoBehaviour
 {
     [SerializeField] ComputeShader simpleScalingCompute;
-    [SerializeField] GameObject prefab;
     [SerializeField] Material material;
     [SerializeField] Mesh mesh;
     [SerializeField][Range(1, 100)] int x_Count;
@@ -12,83 +12,93 @@ public class SimpleScaling : MonoBehaviour
     [SerializeField][Range(1, 100)] int z_Count;
     [SerializeField][Range(0.1f, 10f)] float itemSize;
     [SerializeField] Color itemColor;
+
     struct ItemData
     {
-        Vector3 position;
-        float size;
-        Color color;
-    };
+        public Vector3 position;
+        public float size;
+        public Color color;
+    }
+
+    int maxItemSize = 1000000;
     int spawnCount;
     ComputeBuffer itemBuffer;
     ComputeBuffer argsBuffer;
     int kernel;
-    uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+    uint[] args = new uint[5];
+
     void Start()
     {
         spawnCount = x_Count * y_Count * z_Count;
-        InitializeArg();
-        InitializeBuffers();
-    }
 
+        // Item buffer oluştur
+        int stride = Marshal.SizeOf(typeof(ItemData));
+        itemBuffer = new ComputeBuffer(maxItemSize, stride);
 
-    void InitializeBuffers()
-    {
-        int stride=Marshal.SizeOf(typeof(ItemData));
-        itemBuffer= new ComputeBuffer(spawnCount,stride);
-        ItemData[] initialData=new ItemData[spawnCount];
+        ItemData[] initialData = new ItemData[maxItemSize];
+        for (int i = 0; i < spawnCount; i++)
+        {
+            initialData[i].size = itemSize;
+            initialData[i].color = itemColor;
+        }
         itemBuffer.SetData(initialData);
 
+        // Compute shader kernel
         kernel = simpleScalingCompute.FindKernel("SimpleScaling");
+        simpleScalingCompute.SetBuffer(kernel, "itemBuffer", itemBuffer);
 
-        simpleScalingCompute.SetBuffer(kernel,"itemBuffer", itemBuffer);
-        
         simpleScalingCompute.SetVector("color", itemColor);
         simpleScalingCompute.SetFloat("size", itemSize);
-        simpleScalingCompute.SetInt("x_Count",x_Count);
-        simpleScalingCompute.SetInt("y_Count",y_Count);
-        simpleScalingCompute.SetInt("z_Count",z_Count);
-        int threadGroups=Mathf.CeilToInt(spawnCount/64f);
-        simpleScalingCompute.Dispatch(kernel,threadGroups,1,1);
+        simpleScalingCompute.SetInt("x_Count", x_Count);
+        simpleScalingCompute.SetInt("y_Count", y_Count);
+        simpleScalingCompute.SetInt("z_Count", z_Count);
 
+        int threadGroups = Mathf.CeilToInt(spawnCount / 64f);
+        threadGroups = Mathf.Max(1, threadGroups);
+        simpleScalingCompute.Dispatch(kernel, threadGroups, 1, 1);
+
+        // Indirect draw args buffer
         argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        args[0] = mesh.GetIndexCount(0);
+        args[1] = (uint)spawnCount;
+        args[2] = mesh.GetIndexStart(0);
+        args[3] = mesh.GetBaseVertex(0);
+        args[4] = 0;
         argsBuffer.SetData(args);
+
         material.SetBuffer("itemBuffer", itemBuffer);
-
     }
-    void InitializeArg()
-    {
-        args[0]=mesh.GetIndexCount(0);
-        args[1]=(uint)spawnCount;
-        args[2]=mesh.GetIndexStart(0);
-        args[3]=mesh.GetBaseVertex(0);
-        args[4]=0;
 
-    }
     void Update()
     {
-        if (mesh == null) Debug.LogError("spawnMesh null");
-        if (material == null) Debug.LogError("spawnMaterial null");
-        if (argsBuffer == null) Debug.LogError("argsBuffer null");
+        spawnCount = x_Count * y_Count * z_Count;
+        if (spawnCount > maxItemSize) spawnCount = maxItemSize;
 
-        simpleScalingCompute.SetBuffer(kernel,"itemBuffer", itemBuffer);
-        simpleScalingCompute.SetInt("x_Count",x_Count);
-        simpleScalingCompute.SetInt("y_Count",y_Count);
-        simpleScalingCompute.SetInt("z_Count",z_Count);
+        // Compute shader parametreleri
+        simpleScalingCompute.SetInt("x_Count", x_Count);
+        simpleScalingCompute.SetInt("y_Count", y_Count);
+        simpleScalingCompute.SetInt("z_Count", z_Count);
         simpleScalingCompute.SetFloat("size", itemSize);
-        int threadGroups=Mathf.CeilToInt(spawnCount/64f);
-        simpleScalingCompute.Dispatch(kernel,threadGroups,1,1);
 
-        material.SetFloat("_Size", itemSize);
-        if(argsBuffer==null)
-        {
-            argsBuffer.SetData(args);
-        }
-        Graphics.DrawMeshInstancedIndirect(mesh, 0, material,new Bounds(Vector3.zero, Vector3.one * spawnCount * itemSize), argsBuffer);
+        int threadGroups = Mathf.CeilToInt(spawnCount / 64f);
+        threadGroups = Mathf.Max(1, threadGroups);
+        simpleScalingCompute.Dispatch(kernel, threadGroups, 1, 1);
 
+        // argsBuffer sadece spawnCount değişirse set et
+        args[1] = (uint)spawnCount;
+        argsBuffer.SetData(args);
+
+        // Draw
+        Graphics.DrawMeshInstancedIndirect(mesh, 0, material,
+            new Bounds(Vector3.zero, new Vector3(x_Count, y_Count, z_Count) * itemSize),
+            argsBuffer);
     }
-    void OnDisable(){
+
+    void OnDisable()
+    {
         itemBuffer?.Release();
+        itemBuffer = null;
         argsBuffer?.Release();
+        argsBuffer = null;
     }
-
 }
--- GPU_3D/Assets/_ScalingItems/SimpleScaling.shader
+++ GPU_3D/Assets/_ScalingItems/SimpleScaling.shader
@@ -1,10 +1,7 @@
+
+
 Shader "Unlit/SimpleScaling"
 {
-    Properties
-    {
-        _Color ("Color", Color) = (1,1,1,1)
-        _Size("Size", Float)=1.0
-    }
     SubShader
     {
         Tags { "RenderType"="Opaque" }
@@ -15,9 +12,14 @@ Shader "Unlit/SimpleScaling"
             HLSLPROGRAM
             #pragma vertex vert
             #pragma fragment frag
-            #pragma multi_compile_fog
+
+            #pragma multi_compile_instancing
+            #pragma multi_compile_fwdbase
+            #pragma multi_compile _ _MAIN_LIGHT_SHADOWS _MAIN_LIGHT_SHADOWS_CASCADE _MAIN_LIGHT_SHADOWS_SCREEN
+            #pragma multi_compile _ _SHADOWS_SOFT
 
             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
             struct ItemData
             {
@@ -25,45 +27,66 @@ Shader "Unlit/SimpleScaling"
                 float size;
                 float4 color;
             };
+
             StructuredBuffer<ItemData> itemBuffer;
 
             struct Attributes
             {
-                float4 positionOS : POSITION;
+                float3 positionOS : POSITION;
+                float3 normalOS   : NORMAL;
             };
 
             struct Varyings
             {
                 float4 positionHCS : SV_POSITION;
-                float4 color : COLOR;
+                half4  color       : COLOR;
+                float3 positionWS  : TEXCOORD0;
+                float3 normalWS    : TEXCOORD1;
+                float4 shadowCoord : TEXCOORD2;
             };
-
-            float _Size;
-            float4 _Color;
 
             Varyings vert(Attributes IN, uint instanceID : SV_InstanceID)
             {
                 Varyings OUT;
+
                 ItemData item = itemBuffer[instanceID];
-                
-                OUT.color = item.color;
-    
-                float3 instancePos = itemBuffer[instanceID].position.xyz;
-                
-                float finalScale = _Size;
-                
-                float3 scaledPosOS = IN.positionOS.xyz * finalScale; 
-                
-                float4 finalPos = float4(scaledPosOS + instancePos, 1.0);
-                
-                OUT.positionHCS = TransformObjectToHClip(finalPos);
-                
+
+                float3 scaled    = IN.positionOS * item.size;
+                float3 objectPos = scaled + item.position;
+
+                // Object → World
+                float4 worldPos = mul(GetObjectToWorldMatrix(), float4(objectPos, 1.0));
+
+                OUT.positionWS  = worldPos.xyz;
+                OUT.positionHCS = TransformWorldToHClip(worldPos.xyz);
+                OUT.normalWS    = TransformObjectToWorldNormal(IN.normalOS);
+
+                // ShadowCoord varying
+                OUT.shadowCoord = TransformWorldToShadowCoord(worldPos.xyz);
+
+                OUT.color = (half4)item.color;
                 return OUT;
             }
 
             half4 frag(Varyings IN) : SV_Target
             {
-                return IN.color;
+                half3 albedo     = IN.color.rgb;
+                half3 normalWS   = normalize(IN.normalWS);
+                float3 positionWS = IN.positionWS;
+
+                // Ana ışık + gölge
+                Light mainLight = GetMainLight(IN.shadowCoord);
+
+                // Ambient
+                half3 ambientLight = SampleSH(normalWS);
+
+                // Diffuse
+                half3 diffuse = LightingLambert(mainLight.color, mainLight.direction, normalWS);
+                diffuse *= mainLight.shadowAttenuation;
+
+                half3 final = ambientLight + diffuse;
+
+                return half4(albedo * final, IN.color.a);
             }
 
             ENDHLSL
